5b482f420502b0f8f2fb7e9aa244ed1c52e65d19
Thiago <thiagomunhozmedeiros@gmail.com>
2019-05-20 21:40:44 -0300

    product: add container hoc

---
+++ src/containers/ProductsListContainer.js
@@ -0,0 +1,75 @@
+import React, { useState, useContext } from 'react'
+
+import { StateContext } from '../state'
+
+import Product from '../components/Product/Product'
+import ThumbsList from '../components/Product/ThumbsList'
+import ImageView from '../components/Product/ImageView'
+import Details from '../components/Product/Details'
+
+import formatCurrency from '../utils/formatCurrency'
+
+import { container } from './ProductsListContainer.module.scss'
+
+const ProductsListContainer = () => {
+  const [visibleItem, setVisibleItem] = useState(0)
+  const { state, dispatch } = useContext(StateContext)
+  const { products } = state
+
+  const handleAddToCart = item => {
+    const itemExists = state.cart.items.find(i => i.id === item.id)
+    if (itemExists) return
+
+    dispatch({
+      type: 'addItemToCart',
+      payload: item
+    })
+  }
+
+  const handleThumbClick = clickedItemIndex => setVisibleItem(clickedItemIndex)
+
+  return (
+    <div className={container}>
+      {
+        products.items.map(item => {
+          const {
+            id,
+            images,
+            name,
+            price,
+          } = item.product
+
+          const {
+            installmentValue,
+            installments,
+            value,
+          } = price
+
+          return (
+            <Product id={id} key={name}>
+              <ThumbsList
+                activeItem={visibleItem}
+                images={images}
+                alt={name}
+                onClickItem={handleThumbClick.bind(this)}
+              />
+              <ImageView
+                src={images[visibleItem]}
+                alt={name}
+              />              
+              <Details
+                name={name}
+                installments={installments}
+                installmentValue={formatCurrency(installmentValue)}
+                value={formatCurrency(value)}
+                onClickAddToCart={handleAddToCart.bind(this, item.product)}
+              />
+            </Product>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default React.memo(ProductsListContainer)
