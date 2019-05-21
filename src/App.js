99b01128553cfa2c659d38161b8156bedbe57eb0
Thiago <thiagomunhozmedeiros@gmail.com>
2019-05-20 21:41:43 -0300

    app: update to use containers hocs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import { StateProvider } from './state'
+import './assets/css/index.scss'
 
-const App = () => {
-  return (
-    <div>
-      Let's Rock!
-    </div>
-  )
-}
+const HeaderContainer = lazy(() => import('./containers/HeaderContainer'))
+const ProductsListContainer = lazy(() => import('./containers/ProductsListContainer'))
+const CartContainer = lazy(() => import('./containers/CartContainer'))
+
+const App = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <StateProvider>
+      <HeaderContainer />
+      <CartContainer />
+      <ProductsListContainer />
+    </StateProvider>
+  </Suspense>
+)
 
 export default App
