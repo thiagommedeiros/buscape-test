d28aeca81a2ba79abfc542b44d8a6a959f3d287f
Thiago <thiagomunhozmedeiros@gmail.com>
2019-05-21 16:37:49 -0300

    Updates

---
+++ precache-manifest.9f99827a26ae86db7b092155a874f544.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9164e8cdd90259909366744af89ddb24",
+    "url": "/buscape-test/index.html"
+  },
+  {
+    "revision": "4787be323310bb84e3c0",
+    "url": "/buscape-test/static/css/3.6de64e70.chunk.css"
+  },
+  {
+    "revision": "b4e82f9ec68aedbdcbbd",
+    "url": "/buscape-test/static/css/4.c489bcc4.chunk.css"
+  },
+  {
+    "revision": "34fc14b5e42fbbc43631",
+    "url": "/buscape-test/static/css/5.8bb2fb21.chunk.css"
+  },
+  {
+    "revision": "44ebf033f1db7deda731",
+    "url": "/buscape-test/static/css/6.675a7403.chunk.css"
+  },
+  {
+    "revision": "57e27f40925334a90a64",
+    "url": "/buscape-test/static/css/main.1e172dfa.chunk.css"
+  },
+  {
+    "revision": "af34c37cdcb3b5ffe9de",
+    "url": "/buscape-test/static/js/2.31eb1b68.chunk.js"
+  },
+  {
+    "revision": "4787be323310bb84e3c0",
+    "url": "/buscape-test/static/js/3.6ab2af8c.chunk.js"
+  },
+  {
+    "revision": "b4e82f9ec68aedbdcbbd",
+    "url": "/buscape-test/static/js/4.d6de994e.chunk.js"
+  },
+  {
+    "revision": "34fc14b5e42fbbc43631",
+    "url": "/buscape-test/static/js/5.33567bdf.chunk.js"
+  },
+  {
+    "revision": "44ebf033f1db7deda731",
+    "url": "/buscape-test/static/js/6.0872e02c.chunk.js"
+  },
+  {
+    "revision": "57e27f40925334a90a64",
+    "url": "/buscape-test/static/js/main.5f49380e.chunk.js"
+  },
+  {
+    "revision": "f45bfc0ddd05105eb6eb",
+    "url": "/buscape-test/static/js/runtime~main.d9348453.js"
+  }
+]);
